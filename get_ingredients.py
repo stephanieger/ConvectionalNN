a34142fea6922093afe2541949674df0587d064b
stephanieger <stephanie.ger@gmail.com>
2017-05-05 15:07:04 -0500

    fixed some bugs in the code

--- get_ingredients.py
+++ get_ingredients.py
@@ -5,8 +5,8 @@ import sys
 
 def main():
 
-    start = sys.argv[1]
-    stop = sys.argv[2]
+    start = int(sys.argv[1])
+    stop = int(sys.argv[2])
 
     # parameters
     TIMEOUT = 5.0
@@ -16,15 +16,14 @@ def main():
 
     client = Client(api_id=API_ID, api_key=API_KEY, timeout=TIMEOUT, retries=RETRIES)
 
-    recipeID = pickle.open('recipeID.pkl', 'rb')
+    recipeID = pickle.load(open('recipeID.pkl', 'rb'))
 
     recipeInfo = []
 
-    for i in range(start,stop):
+    for i in range(start, stop):
         recipeInfo += [client.recipe(recipeID[i])]
 
-    pickle.dump(recipeInfo, open('recipeInfo'+str(stop)+'.pkl', 'wb'))
-
+    pickle.dump(recipeInfo, open('recipeInfo'+str(stop-1)+'.pkl', 'wb'))
 if __name__ == '__main__':
     main()
 
