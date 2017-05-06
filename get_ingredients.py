040bcaa524ac4e9a72ae2c1f532465ead01da78f
stephanieger <stephanie.ger@gmail.com>
2017-05-06 12:19:20 -0500

    fixed code so that it actually pulls the right number of recipe IDs

--- get_ingredients.py
+++ get_ingredients.py
@@ -2,17 +2,29 @@ from yummly import Client
 import pickle
 import sys
 
-
 def main():
+    '''
+    This code loops through recipe IDs and uses "get recipe" to call the recipe info for each recipe. It saves the 
+    recipes called periodically just in case an API call fails for some reason in order to minimize the number of wasted
+    API calls. 
+    :param: start - the index at which to start pulling recipe information
+    :param: stop: - the index at which to stop pulling recipe information
+    :return: pkl files with recipe info
+    
+    HOW TO RUN CODE:
+    - fill in your API_ID
+    - fill in you API_KEY
+    - python get_ingredients.py start stop
+    '''
 
     start = int(sys.argv[1])
     stop = int(sys.argv[2])
 
     # parameters
-    TIMEOUT = 5.0
+    TIMEOUT = 10.0
     RETRIES = 0
-    API_ID = 'fill this in'
-    API_KEY = 'fill this in'
+    API_ID = 'fill this in!'
+    API_KEY = 'fill this in!'
 
     client = Client(api_id=API_ID, api_key=API_KEY, timeout=TIMEOUT, retries=RETRIES)
 
@@ -21,9 +33,12 @@ def main():
     recipeInfo = []
 
     for i in range(start, stop):
+        print(i)
         recipeInfo += [client.recipe(recipeID[i])]
+        if (i+1)%50 == 0:
+            pickle.dump(recipeInfo, open('recipeInfo_'+str(i)+'.pkl', 'wb'))
+            recipeInfo = []
 
-    pickle.dump(recipeInfo, open('recipeInfo'+str(stop-1)+'.pkl', 'wb'))
 if __name__ == '__main__':
     main()
 
--- get_recipe.py
+++ get_recipe.py
@@ -2,19 +2,23 @@ from yummly import Client
 import pickle
 
 # parameters
-TIMEOUT = 5.0
+TIMEOUT = 10.0
 RETRIES = 0
-API_ID = 'fill this in'
-API_KEY = 'fill this in'
+API_ID = 'fill this in!'
+API_KEY = 'fill this in!'
 
 client = Client(api_id=API_ID, api_key=API_KEY, timeout=TIMEOUT, retries=RETRIES)
-
-search = client.search('cookie', maxResults=172471)
-
-pickle.dump(search, open('get_recipe_ID.pkl', 'wb'))
-
+searchList = []
 recipeID = []
-for match in search.matches:
-    recipeID += [match.id]
 
-pickle.dump(recipeID, open('recipeID.pkl', 'wb'))
+total_files = 3500
+
+for i in range(total_files):
+    print(i)
+    search = client.search('cookie', maxResults=40, start=i*40)
+    searchList += [search]
+    for match in search.matches:
+        recipeID += [match.id]
+    if (i+1)%10 == 0:
+        pickle.dump(searchList, open('searchList_'+str(i)+'.pkl', 'wb'))
+        pickle.dump(recipeID, open('recipeID_'+str(i)+'.pkl', 'wb'))
