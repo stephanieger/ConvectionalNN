bffde149251d496965728ac7b46dfffc66c80b75
Craig Ng <craigng2017@u.northwestern.edu>
2017-05-08 15:28:54 -0500

    added {} to the code

--- remove_parentheses.py
+++ remove_parentheses.py
@@ -6,5 +6,5 @@ ingredient_list = open('ingredients.txt', 'r')
 # removes any phrases enclosed in either () or []
 with open('ingredients_noparentheses.txt', 'w') as f:
     for line in ingredient_list.readlines():
-        new_line = re.sub("[\(\[].*?[\)\]]", "", line)
+        new_line = re.sub("[\{\(\[].*?[\)\]\}]", "", line)
         f.write(new_line)
